ad437c1df33d4158730f445721eb8a1d53db5152
Onwukaike Martin <nkemakolam.martin@gmail.com>
2024-01-12 23:24:16 +0100

    changed arrOfRequest

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // const { response } = require("express");
 
 // let cache = await caches.open('cache1')
-let arrOfRequest = ['/','index.html', 'script.js', 'style.css', 'sw.js', 'manifest.json', '/images/burger1.png', '/images/burger2.png','/images/burger.jpg', '/images/images.png','/images/no food.png']
+let arrOfRequest = ['/','index.html', 'script.js', 'style.css', 'sw.js', 'manifest.json','/images/burger.jpg', '/images/images.png','/images/no food.png']
 let fetchStrategy = async(reqString)=>{
     let openCache = await caches.open('cache2')
     let matchedCache = await openCache.match(reqString.clone(), {
