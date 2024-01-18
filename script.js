1ea1bc752bf093383f9e30e05398f73aab87edf4
Onwukaike Martin <nkemakolam.martin@gmail.com>
2024-01-18 11:39:44 +0100

    commented out unused code

--- script.js
+++ script.js
@@ -104,128 +104,128 @@ checkoutBtn.onclick =()=>{
     removeCart()
 }
 
-function addToCart(e) {
-    console.log('called')
-    restImg.classList.add('remove')
-        console.log('added')
-        if (data.length > 0) {
-            last.children[last.children.length - 3].remove()
-        }
-        var price = e.target.parentElement.children[0].innerHTML
-        var name = e.target.parentElement.parentElement.children[1].innerHTML
-        var imgSrc = e.target.parentElement.parentElement.children[0].src
-        data.push({
-            price: price,
-            name: name,
-            imgSrc: imgSrc,
-            value: 1
-        })
-        let newCart = document.createElement('div')
-        newCart.classList.add('.bodyHold')
-        htmlMaker(data).forEach((node)=>{
-            newCart.appendChild(node)
-        })
-        cartBody.insertAdjacentElement('afterend', newCart)
-        total.innerHTML = `$${totalFunc()}`
-        console.log(data)
-}
+// function addToCart(e) {
+//     console.log('called')
+//     restImg.classList.add('remove')
+//         console.log('added')
+//         if (data.length > 0) {
+//             last.children[last.children.length - 3].remove()
+//         }
+//         var price = e.target.parentElement.children[0].innerHTML
+//         var name = e.target.parentElement.parentElement.children[1].innerHTML
+//         var imgSrc = e.target.parentElement.parentElement.children[0].src
+//         data.push({
+//             price: price,
+//             name: name,
+//             imgSrc: imgSrc,
+//             value: 1
+//         })
+//         let newCart = document.createElement('div')
+//         newCart.classList.add('.bodyHold')
+//         htmlMaker(data).forEach((node)=>{
+//             newCart.appendChild(node)
+//         })
+//         cartBody.insertAdjacentElement('afterend', newCart)
+//         total.innerHTML = `$${totalFunc()}`
+//         console.log(data)
+// }
 
-cartAdder.forEach((btn)=>{
-        btn.onclick=(e)=>{
-        addToCart(e)
-    }
-})
-
-
+// cartAdder.forEach((btn)=>{
+//         btn.onclick=(e)=>{
+//         addToCart(e)
+//     }
+// })
 
 
-function htmlMaker(dataArg) {
-    let arr = []
 
 
-    dataArg.forEach((object, id)=>{
-        let body = document.createElement('div')
-        body.classList.add('body')
-        let obj = document.createElement('div')
-        obj.classList.add('obj')
-        let h1 = document.createElement('h1')
-        h1.innerText = object.name
-        let img = document.createElement('img')
-        img.src = object.imgSrc
-        let quantity = document.createElement('div')
-        quantity.classList.add('quantity')
-        let buttonAdd = document.createElement('button')
-        buttonAdd.onclick=(e)=>{
-            let multiple
-            let price
-            data = data.map((obj, objID)=>{
-                if (objID === id) {
-                    multiple = obj.value + 1
-                    price = obj.price
-                    return {
-                        price: obj.price,
-                        name: obj.name,
-                        imgSrc: obj.imgSrc,
-                        value: obj.value + 1
-                    }
-                }else{
-                    return obj
-                }
-            })
-            console.log(price)
-            e.target.parentElement.children[1].innerHTML = `${ +e.target.parentElement.children[1].innerHTML + 1}`
-            e.target.parentElement.parentElement.children[3].innerHTML = `$${+price.slice(1) * multiple}`
-            total.innerHTML = `$${totalFunc()}`
-        }
-        buttonAdd.innerText = '+'
-        let buttonRemove = document.createElement('button')
-        buttonRemove.innerText = '-'
-        buttonRemove.onclick=(e)=>{
-            let multiple
-            let price
-            if (+e.target.parentElement.children[1].innerHTML == 1) {
-                console.log('reach')
-                e.target.parentElement.parentElement.parentElement.classList.add('remove')
-                console.log(e.target.parentElement.parentElement.parentElement)
-                removeData(id)
-            }else{ 
-            data = data.map((obj, objID)=>{
-                if (objID === id) {
-                    multiple = obj.value - 1
-                    price = obj.price
-                    return {
-                        price: obj.price,
-                        name: obj.name,
-                        imgSrc: obj.imgSrc,
-                        value: obj.value - 1
-                    }
-                }else{
-                    return obj
-                }
-            })
-            e.target.parentElement.children[1].innerHTML = `${+e.target.parentElement.children[1].innerHTML - 1}`
-            e.target.parentElement.parentElement.children[3].innerHTML = `$${+price.slice(1) * multiple}`
-            }
-            total.innerHTML = `$${totalFunc()}`
-        }
-        let span = document.createElement('span')
-        span.innerText = object.value
-        let p = document.createElement('p')
-        p.innerText = `$${+object.price.slice(1) * object.value}`
+// function htmlMaker(dataArg) {
+//     let arr = []
 
-        quantity.appendChild(buttonAdd)
-        quantity.appendChild(span)
-        quantity.appendChild(buttonRemove)
-        obj.appendChild(img)
-        obj.appendChild(h1)
-        obj.appendChild(quantity)
-        obj.appendChild(p)
-        body.appendChild(obj)
-        arr.push(body)
-    })
+
+//     dataArg.forEach((object, id)=>{
+//         let body = document.createElement('div')
+//         body.classList.add('body')
+//         let obj = document.createElement('div')
+//         obj.classList.add('obj')
+//         let h1 = document.createElement('h1')
+//         h1.innerText = object.name
+//         let img = document.createElement('img')
+//         img.src = object.imgSrc
+//         let quantity = document.createElement('div')
+//         quantity.classList.add('quantity')
+//         let buttonAdd = document.createElement('button')
+//         buttonAdd.onclick=(e)=>{
+//             let multiple
+//             let price
+//             data = data.map((obj, objID)=>{
+//                 if (objID === id) {
+//                     multiple = obj.value + 1
+//                     price = obj.price
+//                     return {
+//                         price: obj.price,
+//                         name: obj.name,
+//                         imgSrc: obj.imgSrc,
+//                         value: obj.value + 1
+//                     }
+//                 }else{
+//                     return obj
+//                 }
+//             })
+//             console.log(price)
+//             e.target.parentElement.children[1].innerHTML = `${ +e.target.parentElement.children[1].innerHTML + 1}`
+//             e.target.parentElement.parentElement.children[3].innerHTML = `$${+price.slice(1) * multiple}`
+//             total.innerHTML = `$${totalFunc()}`
+//         }
+//         buttonAdd.innerText = '+'
+//         let buttonRemove = document.createElement('button')
+//         buttonRemove.innerText = '-'
+//         buttonRemove.onclick=(e)=>{
+//             let multiple
+//             let price
+//             if (+e.target.parentElement.children[1].innerHTML == 1) {
+//                 console.log('reach')
+//                 e.target.parentElement.parentElement.parentElement.classList.add('remove')
+//                 console.log(e.target.parentElement.parentElement.parentElement)
+//                 removeData(id)
+//             }else{ 
+//             data = data.map((obj, objID)=>{
+//                 if (objID === id) {
+//                     multiple = obj.value - 1
+//                     price = obj.price
+//                     return {
+//                         price: obj.price,
+//                         name: obj.name,
+//                         imgSrc: obj.imgSrc,
+//                         value: obj.value - 1
+//                     }
+//                 }else{
+//                     return obj
+//                 }
+//             })
+//             e.target.parentElement.children[1].innerHTML = `${+e.target.parentElement.children[1].innerHTML - 1}`
+//             e.target.parentElement.parentElement.children[3].innerHTML = `$${+price.slice(1) * multiple}`
+//             }
+//             total.innerHTML = `$${totalFunc()}`
+//         }
+//         let span = document.createElement('span')
+//         span.innerText = object.value
+//         let p = document.createElement('p')
+//         p.innerText = `$${+object.price.slice(1) * object.value}`
+
+//         quantity.appendChild(buttonAdd)
+//         quantity.appendChild(span)
+//         quantity.appendChild(buttonRemove)
+//         obj.appendChild(img)
+//         obj.appendChild(h1)
+//         obj.appendChild(quantity)
+//         obj.appendChild(p)
+//         body.appendChild(obj)
+//         arr.push(body)
+//     })
     
-    return arr
-}
+//     return arr
+// }
 
 
 function removeData(arg) {
