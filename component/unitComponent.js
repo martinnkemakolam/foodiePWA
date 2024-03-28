1920da2b14826bb307aeef80751ea30089df81bf
Onwukaike Martin <nkemakolam.martin@gmail.com>
2024-03-28 02:41:57 +0100

    adding dynamic routes

--- component/unitComponent.js
+++ component/unitComponent.js
@@ -128,11 +128,12 @@ elementCreator({
 elementCreator({
     name: "cms-element",
     pugFunc: cms,
-    atr: ['imgsrc', 'name', 'extra', 'price'],
+    atr: ['imgsrc', 'name', 'extra', 'price', 'uid'],
     func: [{
         event: "click",
         callback: function(e){
             if ('delete' in e.target.dataset) {
+                console.log(this.value.uid)
                 controlller.deleteProduct(this.value.uid)
             }else if ('edit' in e.target.dataset) {
                 // route to edit page
--- router.js
+++ router.js
@@ -1,13 +1,28 @@
 export let router = ()=>{
     let views = []
-    let addRoute=(url, htmlString)=>{
-        views.push({url, htmlString})
+    let addRoute=(url, htmlString, isDyn)=>{
+        let urlMatch = url.match(':id')
+        if (urlMatch){
+            let inCompleteRegex = url.slice(0, urlMatch.index)
+            let regex = inCompleteRegex + '(\w+)'   
+            views.push({htmlString, url, regexTest: new RegExp(regex)})
+        }else{
+            views.push({htmlString, url})
+        }
     }
     
     let currentRoute =()=>{
         let app = document.querySelector('#app')
         let {hash} = window.location
-        let current = views.find((e)=> e.url === hash)
+        let current = views.find((e)=>{
+            if (e.regexTest && e.regexTest.test(hash)) {
+                return e
+            }else if (e.url === hash) {
+                return e
+            }{
+                
+            }
+        })
         if (current) {
             app.render(current.htmlString)
         }else{
